8839554a3072b2998c29c6754439085f8fcfa626
Hans Meine <hans_meine@gmx.net>
2003-02-11 17:49:04 +0000

    This commit was generated by cvs2svn to compensate for changes in r11, which included commits to RCS files with non-trunk default branches.

---
+++ src/designer-plugin/widgetplugin.cxx
@@ -0,0 +1,53 @@
+#define QT_PLUGIN
+#include "qimageviewer.hxx"
+#include "fimageviewer.hxx"
+
+#include <qwidgetplugin.h>
+#include <iostream>
+
+class ImageViewersPlugin : public QWidgetPlugin
+{
+  public:
+    virtual QStringList keys() const
+    {
+        return QStringList()
+            << "QImageViewer"
+            << "FImageViewer";
+    }
+
+    virtual QString group (const QString &) const
+    {
+        return "Image Viewers (VIGRA)";
+    }
+
+    virtual QWidget *create(const QString &key,
+                            QWidget *parent = 0, const char *name = 0)
+    {
+        if(key == "QImageViewer")
+            return new QImageViewer(parent, name);
+        if(key == "FImageViewer")
+            return new FImageViewer(parent, name);
+        return NULL;
+    }
+
+    virtual QString includeFile(const QString &key) const
+    {
+        if(key == "QImageViewer")
+            return "qimageviewer.hxx";
+        if(key == "FImageViewer")
+            return "fimageviewer.hxx";
+        return QString::null;
+    }
+
+    virtual QString toolTip(const QString &key) const
+    {
+        if(key == "QImageViewer")
+            return "QImage display widget (zooming, panning..)";
+        if(key == "FImageViewer")
+            return "vigra::FImage display widget (auto-scaling, logarithmic display..)";
+        return QString::null;
+    }
+        //virtual QString whatsThis(const QString &key) const;
+};
+
+Q_EXPORT_PLUGIN(ImageViewersPlugin)
