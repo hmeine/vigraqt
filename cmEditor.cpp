9c4d251f040710a282651c278e61ec7462a55c45
Hans Meine <meine@informatik.uni-hamburg.de>
2005-10-21 15:25:04 +0000

    - improve drawing (outlines) - more general code - add tooltip framework

--- cmEditor.cpp
+++ cmEditor.cpp
@@ -8,6 +8,8 @@ ColorMapEditor::ColorMapEditor(QWidget *parent, const char *name)
 : QWidget(parent, name),
   cm_(NULL)
 {
+	new ColorToolTip(this);
+	gradientRect_.setTopLeft(QPoint(xMargin, yMargin));
 }
 
 void ColorMapEditor::setColorMap(ColorMap *cm)
@@ -17,7 +19,7 @@ void ColorMapEditor::setColorMap(ColorMap *cm)
 
 QSize ColorMapEditor::sizeHint() const
 {
-	return QSize(150, 36);
+	return QSize(240, 33);
 }
 
 void ColorMapEditor::paintEvent(QPaintEvent *e)
@@ -28,23 +30,21 @@ void ColorMapEditor::paintEvent(QPaintEvent *e)
 		return;
 	}
 
-	const int xMargin = 10, yMargin = 2, triangleWidth = 12, triangleHeight = 12;
-
-	double valueOffset = cm_->domainMin(),
-			valueScale = (cm_->domainMax() - cm_->domainMin()) /
-						 (width() - 2*xMargin);
-
+	// set up double buffering and clear pixmap
 	QPixmap pm(size());
 	QPainter p(&pm, this);
-
 	pm.fill(backgroundColor());
 
-	int yBottom = height()-1 - yMargin - triangleHeight + 2;
-	for(int x = xMargin; x < width()-xMargin; ++x)
+	// fill gradientRect_ with gradient and draw outline
+	for(int x = gradientRect_.left(); x <= gradientRect_.right(); ++x)
 	{
-		p.setPen(v2q((*cm_)(valueOffset + valueScale*(x - xMargin))));
-		p.drawLine(x, yMargin, x, yBottom);
+		p.setPen(v2q((*cm_)(valueOffset_ + valueScale_*(x - xMargin))));
+		p.drawLine(x, gradientRect_.top(), x, gradientRect_.bottom());
 	}
+	QRect gradOutline(gradientRect_);
+	gradOutline.addCoords(-1, -1, 1, 1);
+	p.setPen(Qt::black);
+	p.drawRect(gradOutline);
 
 	// setup triangle QPointArray and derive right half from it:
 	QPointArray triangle(3), rightHalf(3);
@@ -59,7 +59,7 @@ void ColorMapEditor::paintEvent(QPaintEvent *e)
 	p.setPen(QPen(Qt::NoPen));
 	for(int i = cm_->size()-1; i >= 0; --i)
 	{
-		int x = (int)((cm_->domainPosition(i)-valueOffset) / valueScale + 0.5);
+		int x = (int)((cm_->domainPosition(i)-valueOffset_) / valueScale_ + 0.5);
 		triangle.translate(xMargin + x - triangle[1].x(), 0);
 		p.setBrush(v2q(cm_->color(i)));
 		p.drawPolygon(triangle);
@@ -68,21 +68,54 @@ void ColorMapEditor::paintEvent(QPaintEvent *e)
 	// draw right halfs of triangles from left to right:
 	for(unsigned int i = 0; i < cm_->size(); ++i)
 	{
-		int x = (int)((cm_->domainPosition(i)-valueOffset) / valueScale + 0.5);
+		int x = (int)((cm_->domainPosition(i)-valueOffset_) / valueScale_ + 0.5);
 		rightHalf.translate(xMargin + x - rightHalf[1].x(), 0);
 		p.setBrush(v2q(cm_->color(i)));
 		p.drawPolygon(rightHalf);
 	}
 
-// 	// draw complete triangle outlines
-// 	p.setPen(Qt::black);
-// 	p.setBrush(Qt::NoBrush);
-// 	for(unsigned int i = 0; i < cm_->size(); ++i)
-// 	{
-// 		int x = (int)((cm_->domainPosition(i)-valueOffset) / valueScale + 0.5);
-// 		triangle.translate(xMargin + x - triangle[1].x(), 0);
-// 		p.drawPolygon(triangle);
-// 	}
+	// draw complete triangle outlines
+	p.setPen(Qt::black);
+	p.setBrush(Qt::NoBrush);
+	for(unsigned int i = 0; i < cm_->size(); ++i)
+	{
+		int x = (int)((cm_->domainPosition(i)-valueOffset_) / valueScale_ + 0.5);
+		triangle.translate(xMargin + x - triangle[1].x(), 0);
+		p.drawPolygon(triangle);
+	}
 
 	bitBlt(this, 0, 0, &pm);
+}
+
+void ColorMapEditor::resizeEvent(QResizeEvent *e)
+{
+	QWidget::resizeEvent(e);
+
+	gradientRect_.setBottomRight(
+		QPoint(width()-1 - xMargin, height()-1 - yMargin - triangleHeight + 2));
+
+	valueOffset_ = cm_->domainMin();
+	valueScale_ = (cm_->domainMax() - cm_->domainMin()) /
+				  (width() - 2*xMargin);
+}
+
+/********************************************************************/
+
+ColorToolTip::ColorToolTip(QWidget *parent)
+: QToolTip(parent)
+{
+}
+
+void ColorToolTip::maybeTip(const QPoint &p)
+{
+    if(!parentWidget()->inherits("ColorMapEditor"))
+        return;
+
+//     QRect r(((ColorMapEditor*)parentWidget())->tip(pos));
+//     if(!r.isValid())
+//         return;
+
+//     QString s;
+//     s.sprintf("position: %d,%d", r.center().x(), r.center().y());
+//     tip(r, s);
 }
