0564230216e42d2acf6338b7aac7da8e8e55adca
Hans Meine <meine@informatik.uni-hamburg.de>
2005-10-21 15:25:04 +0000

    - improve drawing (outlines) - more general code - add tooltip framework

--- cmEditor.h
+++ cmEditor.h
@@ -1,6 +1,7 @@
 #ifndef CMEDITOR_H
 #define CMEDITOR_H
 
+#include <qtooltip.h>
 #include <qwidget.h>
 #include "colormap.hxx"
 
@@ -15,9 +16,26 @@ public:
 	virtual QSize sizeHint() const;
 
 protected:
+	// layout constants:
+	enum { xMargin = 10, yMargin = 2, triangleWidth = 12, triangleHeight = 12 };
+
 	virtual void paintEvent(QPaintEvent *e);
+	virtual void resizeEvent(QResizeEvent *e);
 
 	ColorMap *cm_;
+
+	// dynamic layout values:
+	QRect gradientRect_;
+	double valueOffset_, valueScale_;
+};
+
+class ColorToolTip : public QToolTip
+{
+public:
+    ColorToolTip(QWidget *parent);
+
+protected:
+    void maybeTip(const QPoint &p);
 };
 
 #endif // CMEDITOR_H
