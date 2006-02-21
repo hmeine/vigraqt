548f966977b1e6937cc65c25d032574595519a62
Hans Meine <hans_meine@gmx.net>
2006-02-21 08:54:04 +0000

    remove vigraqt_buildinfo instead of creating it..

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 VERSION=`grep AM_INIT_AUTOMAKE configure.in | sed 's,[^0-9]*\([0-9.a-z]\+\).*,\1,'`
 echo "Preparing VigraQt v$VERSION..."
 
-touch src/sip/vigraqt_buildinfo
+test -f src/sip/vigraqt_buildinfo && rm $_ # remove leftover from old versions
 cat admin/acinclude.m4.in admin/libtool.m4.in > acinclude.m4
 aclocal
 autoheader
