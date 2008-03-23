752923556e143d6421dd1487ce7a2efa3488d42d
Hans Meine <hans_meine@gmx.net>
2008-03-23 20:13:16 +0100

    introduce TransitionIterator and use that to fix painting of step transition triangles

--- src/vigraqt/colormap.hxx
+++ src/vigraqt/colormap.hxx
@@ -77,6 +77,161 @@ class ColorMap
 
     void remove(unsigned int i);
 
+        /**
+         * TransitionIterator is an iterator over groups of transition
+         * points within a ColorMap.  This is similar to an iterator
+         * over each transition point, but it groups together
+         * transition points with the same position (which define
+         * "sharp" step transitions.
+         */
+    class TransitionIterator
+    {
+      public:
+        unsigned int firstIndex() const
+        {
+            return i1_;
+        }
+
+        unsigned int lastIndex() const
+        {
+            return i2_;
+        }
+
+        bool inRange() const
+        {
+            return i1_ < colorMap_->size();
+        }
+
+        bool atEnd() const
+        {
+            return !inRange();
+        }
+
+            /**
+             * Returns true iff there is a sharp transition at this
+             * position.  This is the case if there are multiple
+             * colors defined for the same position, i.e. lastIndex()
+             * != firstIndex().
+             */
+        bool isStepTransition() const
+        {
+            return lastIndex() > firstIndex();
+        }
+
+        double position() const
+        {
+            return colorMap_->position(firstIndex());
+        }
+
+        double domainPosition() const
+        {
+            return colorMap_->domainPosition(firstIndex());
+        }
+
+            /**
+             * Return the color of this transition point on the left
+             * side.  If there are multiple colors defined for the
+             * same position, this returns the first one (i.e. that of
+             * firstIndex()).
+             */
+        Color leftColor() const
+        {
+            return colorMap_->color(firstIndex());
+        }
+
+            /**
+             * Return the color of this transition point on the right
+             * side.  If there are multiple colors defined for the
+             * same position, this returns the last one (i.e. that of
+             * lastIndex()).
+             */
+        Color rightColor() const
+        {
+            return colorMap_->color(lastIndex());
+        }
+
+            /**
+             * Returns true iff the range before this transition point
+             * has a constant color.  This is the case if this is
+             * either the first transition point or the previous
+             * transition point has the same rightColor() as this
+             * leftColor().
+             */
+        bool leftFlat() const
+        {
+            return firstIndex() <= 0 || (
+                colorMap_->color(firstIndex()-1) == leftColor());
+        }
+
+            /**
+             * Returns true iff the range after this transition point
+             * has a constant color.  This is the case if this is
+             * either the last transition point or the next transition
+             * point has the same leftColor() as this rightColor().
+             */
+        bool rightFlat() const
+        {
+            return lastIndex() >= colorMap_->size()-1 || (
+                colorMap_->color(lastIndex()+1) == rightColor());
+        }
+
+        TransitionIterator &operator++()
+        {
+            vigra_assert(!atEnd(), "must not advance if atEnd()!");
+            i1_ = i2_ + 1;
+            advanceRight();
+            return *this;
+        }
+
+        TransitionIterator operator++(int)
+        {
+            TransitionIterator ret(*this);
+            operator++();
+            return ret;
+        }
+
+        bool operator==(TransitionIterator const &other) const
+        {
+            return i1_ == other.i1_;
+        }
+
+        bool operator!=(TransitionIterator const &other) const
+        {
+            return i1_ != other.i1_;
+        }
+
+      protected:
+        friend class ColorMap;
+
+        TransitionIterator(const ColorMap *colorMap, unsigned int index)
+        : colorMap_(colorMap),
+          i1_(index)
+        {
+            advanceRight();
+        }
+
+        void advanceRight()
+        {
+            i2_ = i1_;
+            while(i2_ < colorMap_->size()-1 &&
+                  colorMap_->position(i2_+1) == colorMap_->position(i1_))
+                ++i2_;
+        }
+
+        const ColorMap *colorMap_;
+        unsigned int i1_, i2_;
+    };
+
+    TransitionIterator transitionsBegin() const
+    {
+        return TransitionIterator(this, 0);
+    }
+
+    TransitionIterator transitionsEnd() const
+    {
+        return TransitionIterator(this, size());
+    }
+
   protected:
     void recalculateFactors();
 
