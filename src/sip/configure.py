e04517548113dd38ca03ff81364909bec4dfb5d4
Hans Meine <hans_meine@gmx.net>
2008-04-04 16:02:58 +0200

    update to Qt4

--- src/sip/configure.py
+++ src/sip/configure.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python
-import os, sys
+import os, sys, subprocess
 
-srcdir = "."
-if len(sys.argv) > 1:
-	srcdir = sys.argv[1]
+print "checking SIP/PyQt4 configuration..."
+import PyQt4.pyqtconfig as pyqt4
+config = pyqt4.Configuration()
+#print config
 
-print "checking SIP configuration...",
-import pyqtconfig
-config = pyqtconfig.Configuration()
-print config
+buildfile = "VigraQt.sbf"
+command = [config.sip_bin, "-c", ".", "-b", buildfile] + \
+		  config.pyqt_sip_flags.split() + \
+		  ["-I", config.pyqt_sip_dir, "VigraQtmod.sip"]
 
-print "running SIP..."
-build_file = "vigraqt.sbf"
-os.system(" ".join([
-	config.sip_bin,
-	"-c", ".",
-	"-b", build_file,
-	"-I", config.pyqt_sip_dir,
-	config.pyqt_qt_sip_flags,
-	"vigraqtmod.sip"]))
+print "running SIP (%s)..." % " ".join(command)
+subprocess.call(command)
 
 print "generating Makefile..."
-makefile = pyqtconfig.QtModuleMakefile(config, build_file)
+makefile = pyqt4.QtOpenGLModuleMakefile(config, buildfile)
 # for i in dir(makefile):
 # 	print i
-makefile.extra_libs = ["vigraqt"]
-makefile.extra_include_dirs = [os.path.join(srcdir, "..", "vigraqt")]
-makefile.extra_lib_dirs = [os.path.join(srcdir, "..", "vigraqt")]
+makefile.extra_libs = ["VigraQt"]
+makefile.extra_include_dirs = ["."]
+makefile.extra_lib_dirs = [os.path.join("..", "vigraqt")]
 makefile.generate()
 
 print "done (you can run 'make' now)."
