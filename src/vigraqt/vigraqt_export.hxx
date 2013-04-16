0e07657f9272b095b4ce004e773b5287ed91867d
Hans Meine <hans_meine@gmx.net>
2013-04-16 14:02:02 +0200

    just assume that GCC has visibility __attribute__

--- src/vigraqt/vigraqt_export.hxx
+++ src/vigraqt/vigraqt_export.hxx
@@ -1,6 +1,13 @@
 #ifndef VIGRAQT_EXPORT_HXX
 #define VIGRAQT_EXPORT_HXX
 
+#ifdef __GNUC__
+// just assume that GCC has visibility support (should hold for all
+// reasonably up-to-date versions); this saves us from writing
+// "proper" detection code in .pro files:
+# define HAVE_GCC_VISIBILITY
+#endif
+
 // borrowed from kdemacros.h:
 #ifdef HAVE_GCC_VISIBILITY
 # define _VIGRAQT_NO_EXPORT __attribute__ ((visibility("hidden")))
@@ -15,7 +22,7 @@
 # define _VIGRAQT_EXPORT
 # define _VIGRAQT_IMPORT
 #endif
- 
+
 #ifndef VIGRAQT_EXPORT
 # if defined(MAKE_VIGRAQT_LIB)
    // We are building this library
@@ -25,5 +32,5 @@
 #  define VIGRAQT_EXPORT _VIGRAQT_IMPORT
 # endif
 #endif
- 
+
 #endif // VIGRAQT_EXPORT_HXX
